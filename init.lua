2b19ae282cb91ec9849ea31efe276d9ae773c00d
Garrett Davis <me@garrettdavis.dev>
2025-03-30 16:24:32 -0700

    Initial commit

---
+++ init.lua
@@ -0,0 +1,97 @@
+-- leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.number = true
+vim.opt.showmode = false
+vim.opt.relativenumber = true
+
+vim.schedule(function()
+    vim.opt.clipboard = "unnamedplus"
+end)
+
+vim.g.have_nerd_font = true
+
+vim.opt.breakindent = true
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+
+vim.opt.scrolloff = 2
+
+-- ask for confirmation when doing things like `:q` with unsaved files
+vim.opt.confirm = true
+
+-- colorscheme
+vim.cmd("colorscheme wildcharm")
+
+--== Keymaps ==--
+
+-- clear highlights when exiting search
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
+
+-- diagnostic keymaps
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- buffers
+vim.keymap.set("n", "H", "<cmd>bp<cr>", { desc = "Previous Buffer" })
+vim.keymap.set("n", "L", "<cmd>bn<cr>", { desc = "Next Buffer" })
+
+-- terminal mode exit easily
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- disable arrow keys in normal mode
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+vim.keymap.set("n", "gK", function()
+    local new_config = not vim.diagnostic.config().virtual_lines
+    vim.diagnostic.config({ virtual_lines = new_config })
+end, { desc = "Toggle diagnostic virtual_lines" })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        error("Error cloning lazy.nvim:\n" .. out)
+    end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    { import = "plugins" },
+})
